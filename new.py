fd34cb0a04a82631e1e93d947bef2a449a6b426d
Abhishek-C-2023 <125984011+Abhishek-C-2023@users.noreply.github.com>
2023-02-28 19:23:32 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,13 @@
+x=int(input("Enter first number"))
+y=int(input("Enter second nmber"))
+z=input("Enter the operation need to perform")
+if z=="+":
+    print("answer is",x+y)
+elif z== "-":
+    print("answer is",x-y)
+elif z== "*":
+    print ("answer is ",x*y)
+elif z=="/":
+    print("answer is",x/y)
+else:
+    print("0")
